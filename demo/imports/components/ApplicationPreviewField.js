d35ba93e9f05e9c0fc151b54a84207d5ff3a0d22
Laurent Roger <laurent@laurentroger.com>
2017-08-09 19:57:30 +0200

    fixed render and add onChangeDateTime

--- demo/imports/components/ApplicationPreviewField.js
+++ demo/imports/components/ApplicationPreviewField.js
@@ -7,6 +7,34 @@ import themes from '../lib/themes';
 
 import ApplicationField from './ApplicationField';
 
+const kronosOptions = {
+  color: '#2ECC71',
+  font: 'Roboto',
+  moment: {
+    lang: 'fr',
+  },
+  format: {
+    hour: 'HH:mm',
+  }
+};
+
+const Kronos = props =>
+    <div>
+    <pre>{JSON.stringify({props}, null, 2)}</pre>
+    <Kronos_
+        date={ props.value }
+        options={kronosOptions}
+        format="DD/MM/YYYY"
+        placeholder={ props.value.toString() }
+        onChangeDateTime={ props.onKronosStartDateChange }
+        min={ new Date() }
+        returnAs="JS_DATE"
+    />
+    </div>
+    ;
+
+const KronosField = connectField(Kronos, { ensureValue: false });
+
 class ApplicationPreview extends Component {
     constructor () {
         super(...arguments);
@@ -15,6 +43,9 @@ class ApplicationPreview extends Component {
 
         this.onModel = this.onModel.bind(this);
         this._schema = eval(`(${this.props.value.schema})`);
+
+        this.onKronosStartDateChange = this.onKronosStartDateChange.bind(this);
+
     }
 
     componentWillReceiveProps (props) {
@@ -28,53 +59,17 @@ class ApplicationPreview extends Component {
         this.setState({model: JSON.stringify(model, null, 4)});
     }
 
+    onKronosStartDateChange(date) {
+        console.log("onKronosStartDateChange : ", date);
+        // TODO ???
+    }
+
     render () {
         const Form = themes[this.props.theme].AutoForm;
         const link = styles[this.props.theme];
 
         const props = {...this.props.value};
         props.schema = this._schema;
-
-        const kronosOptions = {
-          color: '#2ECC71',
-          font: 'Roboto',
-          moment: {
-            lang: 'fr',
-          },
-          format: {
-            hour: 'HH:mm',
-          }
-        };
-
-        const Kronos = props =>
-            <div>
-            <pre>{JSON.stringify({props}, null, 2)}</pre>
-            <Kronos_
-                date={ props.value }
-                options={kronosOptions}
-                format="DD/MM/YYYY"
-                placeholder={ props.value }
-                min={ new Date() }
-                returnAs="JS_DATE"
-            />
-            </div>
-            ;
-
-        const KronosField = connectField(Kronos, { ensureValue: false });
-
-        const determineComponentFromProps = props => {
-              console.log(props);
-              return KronosField;
-            };
-
-        const CustomAuto = props => {
-              // This way we don't care about unhandled cases - we use default
-              // AutoField as a fallback component.
-              const Component = determineComponentFromProps(props) || AutoField;
-              return (
-              <Component {...props} />
-          );
-        };
 
         return (
             <div>
@@ -85,7 +80,8 @@ class ApplicationPreview extends Component {
                 ) : (
                     <Form key={this.props.value.schema} onChangeModel={this.onModel} {...props}>
                         <KronosField
-                        name="date"/>
+                        name="date"
+                        onKronosStartDateChange={ this.onKronosStartDateChange } />
                     </Form>
                 )}
 
